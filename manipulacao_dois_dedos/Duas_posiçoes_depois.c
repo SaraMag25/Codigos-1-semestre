a25bd760da3582da93f120e8c4d5c263da2dd362
Sara <saramagalhaes242@gmail.com>
2025-05-28 14:54:49 -0300

    primeiro_desafio

---
+++ manipulacao_dois_dedos/Duas_posiçoes_depois.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define N 10
+
+int L[N] = {1,2,3,4,5,6,7,8,9,10};
+int resp;
+
+int main() {
+    resp = 0; 
+
+    int j = 2;
+    for (int i = 0; j < N; i++, j = i + 2) {
+        if (L[i] < L[j]) {
+            resp = 1;
+            break; 
+        }
+    }
+
+    printf("Resultado: %d\n", resp);
+
+    return 0;
+}
