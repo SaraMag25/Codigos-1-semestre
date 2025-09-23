c042d3ff8f42241f238d6f0514a2e343ea013120
Sara <saramagalhaes242@gmail.com>
2025-09-22 23:26:12 -0300

    Crescente

---
+++ Site_ufc_FUP/Condicional/Crescente.cpp
@@ -0,0 +1,15 @@
+// inclua as bibliotecas e definas as variáveis globais de entrada e saída do modelo
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int a = 5, b = 2, c = 7, d = 8;
+int crescente;
+
+
+
+int main() {
+if (a < b &&  b < c &&  c < d)
+  crescente = 1;
+else crescente = 0;
+}
